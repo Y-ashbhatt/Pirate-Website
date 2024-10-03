536658b9f7ca911242edf37962c92b0fde2c703e
Yash Bhatt <0yashbhatt0@gmail.com>
2024-10-03 22:11:13 +0530

    Add Header and Nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,33 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
 
+// Menu Show 
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+// Menu Close
+
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu');
+    // click on link remove show menu class
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*=============== ADD SHADOW HEADER ===============*/
 
